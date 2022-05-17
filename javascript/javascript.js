bedf46f6ebe2b92a833bd621860c181415d8a74f
thanthuy02 <2051063434@e.tlu.edu.vn>
2022-05-17 12:38:29 +0700

    up code

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById("myMenuDrawer").style.width = "250px";
+}
+
+function closeMenu(){
+    document.getElementById("myMenuDrawer").style.width = "0";
+
+}
